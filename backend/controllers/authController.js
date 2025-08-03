0b6fcc5edc92467329a6b2b1a06eb3e4ac5ddb66
Yatinder <chhoker.yatinder123@gmail.com>
2025-08-03 18:35:47 +0530

    reverting changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax", 
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   path: "/",
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
