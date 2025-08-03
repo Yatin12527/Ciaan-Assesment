add65c07662a695975ebd376a58b5d4a85edf00a
Yatinder <chhoker.yatinder123@gmail.com>
2025-08-03 18:11:25 +0530

    cookie issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "lax", 
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
 
